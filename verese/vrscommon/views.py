3534fcc84c0f8e1b0d3de8aa165e511e7447a160
Giorgos Logiotatidis <seadog@sealabs.net>
2011-09-06 19:14:49 +0300

    add verese 'main' view add request_token_ready function for oauth support

--- verese/vrscommon/views.py
+++ verese/vrscommon/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+
+def request_token_ready(request, token):
+    error = request.GET.get('error', '')
+    ctx = RequestContext(request, {
+        'error' : error,
+        'token' : token
+    })
+    return render_to_response(
+        'piston/request_token_ready.html',
+        context_instance = ctx
+    )
+
+@login_required
+def main(request):
+    return render_to_response('verese.html')
