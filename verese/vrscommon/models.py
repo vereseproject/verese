18b294a829d8bd91464eed257bf8cd3af6404f29
Giorgos Logiotatidis <seadog@sealabs.net>
2011-09-06 16:46:06 +0300

    remove unused validator

--- verese/vrscommon/models.py
+++ verese/vrscommon/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
 from django.db.models import Sum, Q
 
