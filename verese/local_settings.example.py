565473ba9939e7a8719c9e38f133ac6aec85594f
Giorgos Logiotatidis <seadog@sealabs.net>
2011-09-07 16:59:27 +0300

    comment on INNODB for mysql

--- verese/local_settings.example.py
+++ verese/local_settings.example.py
@@ -8,6 +8,10 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        # when using mysql you need INNODB to enable transactions
+        # 'OPTIONS': {
+        #            'init_command': 'SET storage_engine=INNODB',
+        #            }
     }
 }
 
