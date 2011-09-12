a22897b0677c66a99c7aa5d948b8c1bb91a917e2
Giorgos Logiotatidis <seadog@sealabs.net>
2011-09-12 21:33:40 +0300

    added welcome page

--- verese/vrscommon/urls.py
+++ verese/vrscommon/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns(
     'vrscommon.views',
     url(r'^$', 'main'),
+    url(r'^welcome/$', 'welcome'),
     )
 
--- verese/vrscommon/views.py
+++ verese/vrscommon/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
+from vrscommon.models import Currency
+
 def request_token_ready(request, token):
     error = request.GET.get('error', '')
     ctx = RequestContext(request, {
@@ -17,3 +19,10 @@ def request_token_ready(request, token):
 @login_required
 def main(request):
     return render_to_response('verese.html')
+
+@login_required
+def welcome(request):
+    ctx = {
+        'currencies':Currency.objects.all()
+        }
+    return render_to_response('welcome.html', ctx)
