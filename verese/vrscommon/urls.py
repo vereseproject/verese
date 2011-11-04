7ce5262745bef437bfd3e2b1746b5f8d9a832631
Giorgos Logiotatidis <seadog@sealabs.net>
2011-11-04 14:42:41 +0200

    redirect everything to main view, also login, logout

--- verese/vrscommon/urls.py
+++ verese/vrscommon/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns(
-    'vrscommon.views',
-    url(r'^$', 'main'),
+    '',
+    # url(r'^login$',
+    #     'django.contrib.auth.views.login',
+    #     {'template_name': 'verese.html'},
+    #     name="login"),
+    # url(r'^logout$',
+    #     'django.contrib.auth.views.logout_then_login',
+    #     name="logout"),
+    url(r'^', 'vrscommon.views.main', name="vereseapp"),
     )
 
