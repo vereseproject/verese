b0691eafb62ca61cc22e6c7154e775fe8e8fbdcb
Giorgos Logiotatidis <seadog@sealabs.net>
2011-11-17 18:06:44 +0200

    removed login/logout url entries

--- verese/vrscommon/urls.py
+++ verese/vrscommon/urls.py
@@ -2,13 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    # url(r'^login$',
-    #     'django.contrib.auth.views.login',
-    #     {'template_name': 'verese.html'},
-    #     name="login"),
-    # url(r'^logout$',
-    #     'django.contrib.auth.views.logout_then_login',
-    #     name="logout"),
     url(r'^', 'vrscommon.views.main', name="vereseapp"),
     )
 
