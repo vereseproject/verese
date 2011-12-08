40197116165bd5768485c8cbcb19a61d214b1fcf
Giorgos Logiotatidis <seadog@sealabs.net>
2011-12-08 14:13:16 -0800

    exchange slider with text for now

--- verese/media/js/verese-new.js
+++ verese/media/js/verese-new.js
@@ -73,6 +73,13 @@ function initialize_addpeople_page() {
 					   checked: checked
 				       });
 		  	});
+
+		  // scroll event handling
+		  // $('#slider-' + user.id).siblings('.ui-slider').bind('touchstart', sliderTouchStart);
+		  // $('#slider-' + user.id).siblings('.ui-slider').bind('mousedown', sliderTouchStart);
+		  // $('#slider-' + user.id).siblings('.ui-slider').bind('mouseup', sliderTouchEnd);
+		  // $('#slider-' + user.id).siblings('.ui-slider').bind('touchend', sliderTouchEnd);
+
 		  $('#AddPeopleListItem').tmpl(ddata).appendTo('#add_people_list');
 		  $('#add_people_list').listview("refresh");
 	      });
@@ -81,38 +88,6 @@ function initialize_addpeople_page() {
 // function to initiailize #add page
 function initialize_add_page() {
     var users = [];
-
-
-    $('#add-search-field').bind('focus', function() { this.value='' });
-
-    $('#add-search-button').bind('click', function(event) {
-				   user = window.user_dict[$('#add-search-field').attr('value')];
-				   $('#add-search-field').attr('value', 'Add another person');
-				   ddata = [{
-						'name': get_full_name(user),
-						'md5': user.emailmd5,
-						'id': user.id
-					    }];
-				     $('#addVeresedakiParticipant').tmpl(ddata).appendTo('#peoplelist');
-				     $('#peoplelist').trigger('create');
-
-				     if ($('.veresedaki_participant').length == 1)
-				     	 $('.veresedaki_participant_slider').slider('disable');
-				     else
-				     	 $('.veresedaki_participant_slider').slider('enable');
-
-				     $("#peoplelist").listview("refresh");
-
-				     $('#slider-' + user.id).siblings('.ui-slider').bind('touchstart', sliderTouchStart);
-				     $('#slider-' + user.id).siblings('.ui-slider').bind('mousedown', sliderTouchStart);
-				     $('#slider-' + user.id).siblings('.ui-slider').bind('mouseup', sliderTouchEnd);
-				     $('#slider-' + user.id).siblings('.ui-slider').bind('touchend', sliderTouchEnd);
-
-				     update_veresedaki_sliders();
-
-
-				     $('#add-search-field').attr('value', 'Add another person');
-			       });
 
     // update sliders on #sumfield change
     $('#sumfield').change(function() {
@@ -159,35 +134,36 @@ function initialize_add_page() {
 
 }
 
-function sliderTouchStart(event) {
-    window._slider_starting_value = $(event.target).closest('.veresedaki_participant').find('.veresedaki_participant_slider').val();
-    window._slider_item = $(event.target).closest('.veresedaki_participant').find('.veresedaki_participant_slider');
+// function sliderTouchStart(event) {
+//     window._slider_starting_value = $(event.target).closest('.veresedaki_participant').find('.veresedaki_participant_slider').val();
+//     window._slider_item = $(event.target).closest('.veresedaki_participant').find('.veresedaki_participant_slider');
 
-}
+// }
 
-function sliderTouchEnd(event) {
-    diff = window._slider_item.val() - window._slider_starting_value;
+// function sliderTouchEnd(event) {
+//     diff = window._slider_item.val() - window._slider_starting_value;
 
-    each_diff = diff / ($('.veresedaki_participant_slider').length - 1);
+//     each_diff = diff / ($('.veresedaki_participant_slider').length - 1);
 
-    $('.veresedaki_participant_slider').each(function(index, item) {
-						 if (item != window._slider_item)
-						     $(item).val(item.value - each_diff);
-					     });
+//     $('.veresedaki_participant_slider').each(function(index, item) {
+// 						 if (item != window._slider_item)
+// 						     $(item).val(item.value - each_diff);
+// 					     });
 
-    $('.veresedaki_participant_slider').slider('refresh');
+//     $('.veresedaki_participant_slider').slider('refresh');
 
-    window._slider_starting_value = null;
-    window._slider_item = null;
-}
+//     window._slider_starting_value = null;
+//     window._slider_item = null;
+// }
 
 function update_veresedaki_sliders() {
     sum_value = $('#sumfield').attr('value') / $('.veresedaki_participant').length;
 
-    $('.veresedaki_participant_slider').attr('value', sum_value);
-    $('.veresedaki_participant_slider').attr('max', $('#sumfield').attr('value'));
+    // $('.veresedaki_participant_slider').attr('value', sum_value);
+    // $('.veresedaki_participant_slider').attr('max', $('#sumfield').attr('value'));
 
-    $('.veresedaki_participant_slider').slider('refresh');
+    // $('.veresedaki_participant_slider').slider('refresh');
+    $('.veresedaki_participant_slider').html(sum_value);
 }
 
 // function to initialize #login page
